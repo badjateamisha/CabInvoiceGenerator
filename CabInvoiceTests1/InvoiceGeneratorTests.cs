5e34895de62da1c5da3c82605d3eeaef50ef425f
badjateamisha <amishabadjate175@gmail.com>
2022-06-03 09:33:02 +0530

    Test case for fare calculation

---
+++ CabInvoiceTests1/InvoiceGeneratorTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CabInvoice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoice.Tests
+{
+    [TestClass()]
+    public class InvoiceGeneratorTests
+    {
+        [TestMethod()]
+        public void CalculateFareTest()
+        {
+            InvoiceGenerator uc1 = new();
+            double fare1 = uc1.CalculateFare(10, 30);
+            Assert.AreEqual(130, fare1);
+        }
+    }
+}
